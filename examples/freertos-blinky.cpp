58761295e52ea6d05ddd40ba44013d0d378d5d00
Marti Bolivar <mbolivar@leaflabs.com>
2011-06-01 11:43:15 -0400

    examples: Add freertos-blinky.cpp.
    Adapted this for whitespace and C++:
    
    http://akb77.com/g/mcu/maple-stm32-library-for-freertos/

---
+++ examples/freertos-blinky.cpp
@@ -0,0 +1,43 @@
+#include "wirish.h"
+#include "libraries/MapleFreeRTOS.h"
+
+static void vLEDFlashTask( void *pvParameters ) {
+    for(;;) {
+        vTaskDelay(2000);
+        digitalWrite(BOARD_LED_PIN, HIGH);
+        vTaskDelay(200);
+        digitalWrite(BOARD_LED_PIN, LOW);
+    }
+}
+
+void setup() {
+    // initialize the digital pin as an output:
+    pinMode(BOARD_LED_PIN, OUTPUT);
+
+    xTaskCreate(vLEDFlashTask,
+                "LEDx",
+                configMINIMAL_STACK_SIZE,
+                NULL,
+                tskIDLE_PRIORITY + 2,
+                NULL);
+    vTaskStartScheduler();
+}
+
+void loop() {
+    // Insert background code here
+}
+
+// Force init to be called *first*, i.e. before static object allocation.
+// Otherwise, statically allocated objects that need libmaple may fail.
+__attribute__((constructor)) void premain() {
+    init();
+}
+
+int main(void) {
+    setup();
+
+    while (true) {
+        loop();
+    }
+    return 0;
+}
