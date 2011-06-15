82b07cbaa4ef771cf86647d964a755b1d05704b5
Marti Bolivar <mbolivar@leaflabs.com>
2011-06-15 17:26:23 -0400

    usb: Remove usages of ST-style NVIC_TypeDef (and NVIC_BASE_ADDR).
    Replace with NVIC API.

--- libmaple/usb/usb_hardware.c
+++ libmaple/usb/usb_hardware.c
@@ -31,6 +31,7 @@
  *
  */
 
+#include "nvic.h"
 #include "usb_hardware.h"
 
 void nvicInit(NVIC_InitTypeDef* NVIC_InitStruct) {
@@ -41,8 +42,6 @@ void nvicInit(NVIC_InitTypeDef* NVIC_InitStruct) {
     u32 tmpsub      = 0x0F;
 
     SCB_TypeDef* rSCB = (SCB_TypeDef *) SCB_BASE_ADDR;
-    NVIC_TypeDef* rNVIC = (NVIC_TypeDef *) NVIC_BASE_ADDR;
-
 
     /* Compute the Corresponding IRQ Priority -------------------------------*/
     tmppriority = (0x700 - (rSCB->AIRCR & (u32)0x700))>> 0x08;
@@ -57,26 +56,24 @@ void nvicInit(NVIC_InitTypeDef* NVIC_InitStruct) {
     tmppriority = ((u32)tmppriority) <<
         ((NVIC_InitStruct->NVIC_IRQChannel & (u8)0x03) * 0x08);
 
-    tmpreg = rNVIC->IPR[(NVIC_InitStruct->NVIC_IRQChannel >> 0x02)];
+    tmpreg = NVIC_BASE->IP[(NVIC_InitStruct->NVIC_IRQChannel >> 0x02)];
     tmpmask = (u32)0xFF <<
         ((NVIC_InitStruct->NVIC_IRQChannel & (u8)0x03) * 0x08);
     tmpreg &= ~tmpmask;
     tmppriority &= tmpmask;
     tmpreg |= tmppriority;
 
-    rNVIC->IPR[(NVIC_InitStruct->NVIC_IRQChannel >> 0x02)] = tmpreg;
+    NVIC_BASE->IP[(NVIC_InitStruct->NVIC_IRQChannel >> 0x02)] = tmpreg;
 
     /* Enable the Selected IRQ Channels -------------------------------------*/
-    rNVIC->ISER[(NVIC_InitStruct->NVIC_IRQChannel >> 0x05)] =
+    NVIC_BASE->ISER[(NVIC_InitStruct->NVIC_IRQChannel >> 0x05)] =
         (u32)0x01 << (NVIC_InitStruct->NVIC_IRQChannel & (u8)0x1F);
 }
 
 void nvicDisableInterrupts() {
-    NVIC_TypeDef* rNVIC = (NVIC_TypeDef *) NVIC_BASE_ADDR;
-    rNVIC->ICER[0] = 0xFFFFFFFF;
-    rNVIC->ICER[1] = 0xFFFFFFFF;
-    rNVIC->ICPR[0] = 0xFFFFFFFF;
-    rNVIC->ICPR[1] = 0xFFFFFFFF;
+    nvic_irq_disable_all();
+    NVIC_BASE->ICPR[0] = 0xFFFFFFFF;
+    NVIC_BASE->ICPR[1] = 0xFFFFFFFF;
 
     /* Disable the systick timer, which operates separately from NVIC */
     SET_REG(STK_CTRL,0x04);
