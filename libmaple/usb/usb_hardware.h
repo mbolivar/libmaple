0668d916e9cb0c1768f43c49d994cbc70ed2d553
Marti Bolivar <mbolivar@leaflabs.com>
2011-06-20 17:34:25 -0400

    usb_hardware.h: Remove unused code.

--- libmaple/usb/usb_hardware.h
+++ libmaple/usb/usb_hardware.h
@@ -32,32 +32,6 @@
 #define EXC_RETURN 0xFFFFFFF9
 #define DEFAULT_CPSR 0x61000000
 
-#define FLASH ((u32)0x40022000)
-
-#define USB_PACKET_BUFFER ((u32)0x40006000)
-
-#define SCS      0xE000E000
-#define SCB      (SCS+0xD00)
-#define STK      (SCS+0x10)
-
-#define SCB_VTOR (SCB+0x08)
-#define STK_CTRL (STK+0x00)
-
-#define USB_HP_IRQ  ((u8)0x13)
-#define USB_LP_IRQ  ((u8)0x14)
-
-/* AIRCR  */
-#define AIRCR_RESET         0x05FA0000
-#define AIRCR_RESET_REQ     (AIRCR_RESET | (u32)0x04);
-
-/* temporary copyage of example from kiel */
-#define __VAL(__TIMCLK, __PERIOD) ((__TIMCLK/1000000UL)*__PERIOD)
-#define __PSC(__TIMCLK, __PERIOD)  (((__VAL(__TIMCLK, __PERIOD)+49999UL)/50000UL) - 1)
-#define __ARR(__TIMCLK, __PERIOD) ((__VAL(__TIMCLK, __PERIOD)/(__PSC(__TIMCLK, __PERIOD)+1)) - 1)
-
-#define SET_REG(addr,val)  do { *(vu32*)(addr)=val; } while (0)
-#define GET_REG(addr)      do { *(vu32*)(addr); } while (0)
-
 #if defined(__cplusplus)
 extern "C" {
 #endif
