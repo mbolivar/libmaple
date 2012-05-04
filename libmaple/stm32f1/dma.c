a5b05d398298b383641d2fb79d9ce1dac9230491
Marti Bolivar <mbolivar@leaflabs.com>
2012-05-03 21:41:31 -0400

    [UNTESTED] Add new "tube"-based DMA interface.
    ***NEW DMA INTERFACE***. Needs testing and experimentation in a
    variety of common use cases before we commit to it.
    
    STM32F1 conducts DMAed bytes through channels 1--7. STM32F2 conducts
    them through streams, where stream listens to one of channels 0--7.
    There is otherwise a close correspondence between channels and
    streams: each is configured with a peripheral and memory address, a
    number of data to transfer, and other settings (memory/peripheral
    address post-increment, etc.). The STM32F2 versions are more
    general (supporting double buffering and FIFOs, etc.), but otherwise
    there is a direct analogy beween channels on F1 and streams on F2 (the
    fact that F2 reused the word "channels" to describe what streams
    listen to is confusing and unfortunate).
    
    To help present a simple abstraction, we introduce the type
    "dma_tube", which stands for whatever you configure to set up a DMA
    transfer. On STM32F1, a dma_tube is a dma_channel. On STM32F2, it will
    be a dma_stream. You can configure a dma_tube using the function
    dma_tube_cfg(), which each series must implement.
    
    This abstraction (along with some others, notably dma_request_src and
    dma_mode_flags, described in comments in the patch to
    <libmaple/dma.h>) lets us make a DMA interface that will hopefully
    help portability between the two series. It's true that code using DMA
    is, in the end, nonportable, but large chunks of it can be abstracted
    out, with the nonportable bits hopefully mostly relegated to data
    rather than code.
    
    Implement this new abstraction on F1, keeping the old names (which use
    "channel" instead of "tube") around for backwards compatibility. Also
    update for XL-density and fix some DMA2 CH4/CH5 bugs we've had lying
    around for a while. (This really belongs in another commit...).
    
    Add <libmaple/dma_common.h> for things that <libmaple/dma.h> and
    <series/dma.h> both need. This prevents us from putting things into
    the libmaple header, then including the series header, which is an
    ugly hack we've used elsewhere and should retroactively fix.
    
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- libmaple/stm32f1/dma.c
+++ libmaple/stm32f1/dma.c
@@ -50,7 +50,7 @@ static dma_dev dma1 = {
                  { .handler = NULL, .irq_line = NVIC_DMA_CH6 },
                  { .handler = NULL, .irq_line = NVIC_DMA_CH7 }},
 };
-/** DMA1 device */
+/** STM32F1 DMA1 device */
 dma_dev *DMA1 = &dma1;
 
 #if defined(STM32_HIGH_DENSITY) || defined(STM32_XL_DENSITY)
@@ -63,7 +63,7 @@ static dma_dev dma2 = {
                  { .handler = NULL, .irq_line = NVIC_DMA2_CH_4_5 },
                  { .handler = NULL, .irq_line = NVIC_DMA2_CH_4_5 }}, /* !@#$ */
 };
-/** DMA2 device */
+/** STM32F1 DMA2 device */
 dma_dev *DMA2 = &dma2;
 #endif
 
@@ -71,75 +71,30 @@ dma_dev *DMA2 = &dma2;
  * Routines
  */
 
-/**
- * @brief Set up a DMA transfer.
- *
- * The channel will be disabled before being reconfigured.  The
- * transfer will have low priority by default.  You may choose another
- * priority before the transfer begins using dma_set_priority(), as
- * well as performing any other configuration you desire.  When the
- * channel is configured to your liking, enable it using dma_enable().
- *
- * @param dev DMA device.
- * @param channel DMA channel.
- * @param peripheral_address Base address of peripheral data register
- *                           involved in the transfer.
- * @param peripheral_size Peripheral data transfer size.
- * @param memory_address Base memory address involved in the transfer.
- * @param memory_size Memory data transfer size.
- * @param mode Logical OR of dma_mode_flags
- * @sideeffect Disables the given DMA channel.
- * @see dma_xfer_size
- * @see dma_mode_flags
- * @see dma_set_num_transfers()
- * @see dma_set_priority()
- * @see dma_attach_interrupt()
- * @see dma_enable()
- */
-void dma_setup_transfer(dma_dev       *dev,
-                        dma_channel    channel,
-                        __io void     *peripheral_address,
-                        dma_xfer_size  peripheral_size,
-                        __io void     *memory_address,
-                        dma_xfer_size  memory_size,
-                        uint32         mode) {
-    dma_channel_reg_map *channel_regs = dma_channel_regs(dev, channel);
-
-    dma_disable(dev, channel);  /* can't write to CMAR/CPAR otherwise */
-    channel_regs->CCR = (memory_size << 10) | (peripheral_size << 8) | mode;
-    channel_regs->CMAR = (uint32)memory_address;
-    channel_regs->CPAR = (uint32)peripheral_address;
-}
-
-/**
- * @brief Set the number of data to be transferred on a DMA channel.
- *
- * You may not call this function while the channel is enabled.
- *
- * @param dev DMA device
- * @param channel Channel through which the transfer occurs.
- * @param num_transfers
- */
-void dma_set_num_transfers(dma_dev *dev,
-                           dma_channel channel,
-                           uint16 num_transfers) {
+int dma_tube_cfg(dma_dev *dev, dma_channel channel, dma_tube_config *config) {
     dma_channel_reg_map *channel_regs;
 
-    ASSERT_FAULT(!dma_is_channel_enabled(dev, channel));
+    /* Make sure config specifies a request source that dev/channel
+     * can actually serve. If not, the request is impossible. If so,
+     * we can otherwise safely ignore tube_req_src on this series. */
+    if (config->tube_req_src != (dma_request_src)((dev->clk_id << 3) |
+                                                  channel)) {
+        ASSERT(0);
+        return -1;
+    }
+
+    dma_disable(dev, channel);
 
     channel_regs = dma_channel_regs(dev, channel);
-    channel_regs->CNDTR = num_transfers;
+    channel_regs->CCR = ((config->tube_mem_size << 10) |
+                         (config->tube_per_size << 8) |
+                         config->tube_mode);
+    channel_regs->CNDTR = config->tube_nr_xfers;
+    channel_regs->CMAR = (uint32)config->tube_mem_addr;
+    channel_regs->CPAR = (uint32)config->tube_per_addr;
+    return 0;
 }
 
-/**
- * @brief Set the priority of a DMA transfer.
- *
- * You may not call this function while the channel is enabled.
- *
- * @param dev DMA device
- * @param channel DMA channel
- * @param priority priority to set.
- */
 void dma_set_priority(dma_dev *dev,
                       dma_channel channel,
                       dma_priority priority) {
@@ -155,130 +110,73 @@ void dma_set_priority(dma_dev *dev,
     channel_regs->CCR = ccr;
 }
 
-/**
- * @brief Attach an interrupt to a DMA transfer.
- *
- * Interrupts are enabled using appropriate mode flags in
- * dma_setup_transfer().
- *
- * @param dev DMA device
- * @param channel Channel to attach handler to
- * @param handler Interrupt handler to call when channel interrupt fires.
- * @see dma_setup_transfer()
- * @see dma_get_irq_cause()
- * @see dma_detach_interrupt()
- */
-void dma_attach_interrupt(dma_dev *dev,
-                          dma_channel channel,
+void dma_set_num_transfers(dma_dev *dev,
+                           dma_channel channel,
+                           uint16 num_transfers) {
+    dma_channel_reg_map *channel_regs;
+
+    ASSERT_FAULT(!dma_is_channel_enabled(dev, channel));
+
+    channel_regs = dma_channel_regs(dev, channel);
+    channel_regs->CNDTR = num_transfers;
+}
+
+void dma_attach_interrupt(dma_dev *dev, dma_channel channel,
                           void (*handler)(void)) {
     dev->handlers[channel - 1].handler = handler;
     nvic_irq_enable(dev->handlers[channel - 1].irq_line);
 }
 
-/**
- * @brief Detach a DMA transfer interrupt handler.
- *
- * After calling this function, the given channel's interrupts will be
- * disabled.
- *
- * @param dev DMA device
- * @param channel Channel whose handler to detach
- * @sideeffect Clears interrupt enable bits in the channel's CCR register.
- * @see dma_attach_interrupt()
- */
 void dma_detach_interrupt(dma_dev *dev, dma_channel channel) {
     /* Don't use nvic_irq_disable()! Think about DMA2 channels 4 and 5. */
     dma_channel_regs(dev, channel)->CCR &= ~0xF;
     dev->handlers[channel - 1].handler = NULL;
 }
 
-/**
- * @brief Discover the reason why a DMA interrupt was called.
- *
- * You may only call this function within an attached interrupt
- * handler for the given channel.
- *
- * This function resets the internal DMA register state which encodes
- * the cause of the interrupt; consequently, it can only be called
- * once per interrupt handler invocation.
- *
- * @param dev DMA device
- * @param channel Channel whose interrupt is being handled.
- * @return Reason why the interrupt fired.
- * @sideeffect Clears channel status flags in dev->regs->ISR.
- * @see dma_attach_interrupt()
- * @see dma_irq_cause
- */
+void dma_enable(dma_dev *dev, dma_channel channel) {
+    dma_channel_reg_map *chan_regs = dma_channel_regs(dev, channel);
+    bb_peri_set_bit(&chan_regs->CCR, DMA_CCR_EN_BIT, 1);
+}
+
+void dma_disable(dma_dev *dev, dma_channel channel) {
+    dma_channel_reg_map *chan_regs = dma_channel_regs(dev, channel);
+    bb_peri_set_bit(&chan_regs->CCR, DMA_CCR_EN_BIT, 0);
+}
+
 dma_irq_cause dma_get_irq_cause(dma_dev *dev, dma_channel channel) {
+    /* Grab and clear the ISR bits. */
     uint8 status_bits = dma_get_isr_bits(dev, channel);
+    dma_clear_isr_bits(dev, channel);
 
     /* If the channel global interrupt flag is cleared, then
      * something's very wrong. */
-    ASSERT(status_bits & BIT(0));
-
-    dma_clear_isr_bits(dev, channel);
+    ASSERT(status_bits & 0x1);
+    /* If GIF is set, then some other flag should be set, barring
+     * something unexpected (e.g. the user making an unforeseen IFCR
+     * write). */
+    ASSERT(status_bits != 0x1);
 
     /* ISR flags get set even if the corresponding interrupt enable
      * bits in the channel's configuration register are cleared, so we
      * can't use a switch here.
      *
      * Don't change the order of these if statements. */
-    if (status_bits & BIT(3)) {
+    if (status_bits & 0x8) {
         return DMA_TRANSFER_ERROR;
-    } else if (status_bits & BIT(1)) {
+    } else if (status_bits & 0x2) {
         return DMA_TRANSFER_COMPLETE;
-    } else if (status_bits & BIT(2)) {
+    } else if (status_bits & 0x4) {
         return DMA_TRANSFER_HALF_COMPLETE;
-    } else if (status_bits & BIT(0)) {
-        /* Shouldn't happen (unless someone messed up an IFCR write). */
-        throb();
     }
-#if DEBUG_LEVEL < DEBUG_ALL
-    else {
-        /* We shouldn't have been called, but the debug level is too
-         * low for the above ASSERT() to have had any effect.  In
-         * order to fail fast, mimic the DMA controller's behavior
-         * when an error occurs. */
-        dma_disable(dev, channel);
-    }
-#endif
+
+    /* If we get here, one of our assumptions has been violated, but
+     * the debug level is too low for the above ASSERTs() to have had
+     * any effect. In order to fail fast, mimic the DMA controller's
+     * behavior when an error occurs. */
+    dma_disable(dev, channel);
     return DMA_TRANSFER_ERROR;
 }
 
-/**
- * @brief Enable a DMA channel.
- * @param dev DMA device
- * @param channel Channel to enable
- */
-void dma_enable(dma_dev *dev, dma_channel channel) {
-    dma_channel_reg_map *chan_regs = dma_channel_regs(dev, channel);
-    bb_peri_set_bit(&chan_regs->CCR, DMA_CCR_EN_BIT, 1);
-}
-
-/**
- * @brief Disable a DMA channel.
- * @param dev DMA device
- * @param channel Channel to disable
- */
-void dma_disable(dma_dev *dev, dma_channel channel) {
-    dma_channel_reg_map *chan_regs = dma_channel_regs(dev, channel);
-    bb_peri_set_bit(&chan_regs->CCR, DMA_CCR_EN_BIT, 0);
-}
-
-/**
- * @brief Set the base memory address where data will be read from or
- *        written to.
- *
- * You must not call this function while the channel is enabled.
- *
- * If the DMA memory size is 16 bits, the address is automatically
- * aligned to a half-word.  If the DMA memory size is 32 bits, the
- * address is aligned to a word.
- *
- * @param dev DMA Device
- * @param channel Channel whose base memory address to set.
- * @param addr Memory base address to use.
- */
 void dma_set_mem_addr(dma_dev *dev, dma_channel channel, __io void *addr) {
     dma_channel_reg_map *chan_regs;
 
@@ -288,20 +186,6 @@ void dma_set_mem_addr(dma_dev *dev, dma_channel channel, __io void *addr) {
     chan_regs->CMAR = (uint32)addr;
 }
 
-/**
- * @brief Set the base peripheral address where data will be read from
- *        or written to.
- *
- * You must not call this function while the channel is enabled.
- *
- * If the DMA peripheral size is 16 bits, the address is automatically
- * aligned to a half-word.  If the DMA peripheral size is 32 bits, the
- * address is aligned to a word.
- *
- * @param dev DMA Device
- * @param channel Channel whose peripheral data register base address to set.
- * @param addr Peripheral memory base address to use.
- */
 void dma_set_per_addr(dma_dev *dev, dma_channel channel, __io void *addr) {
     dma_channel_reg_map *chan_regs;
 
@@ -309,6 +193,52 @@ void dma_set_per_addr(dma_dev *dev, dma_channel channel, __io void *addr) {
 
     chan_regs = dma_channel_regs(dev, channel);
     chan_regs->CPAR = (uint32)addr;
+}
+
+/**
+ * @brief Deprecated. Use dma_tube_cfg() instead.
+ *
+ * Set up a DMA transfer.
+ *
+ * The channel will be disabled before being reconfigured.  The
+ * transfer will have low priority by default.  You may choose another
+ * priority before the transfer begins using dma_set_priority(), as
+ * well as performing any other configuration you desire.  When the
+ * channel is configured to your liking, enable it using dma_enable().
+ *
+ * @param dev DMA device.
+ * @param channel DMA channel.
+ * @param peripheral_address Base address of peripheral data register
+ *                           involved in the transfer.
+ * @param peripheral_size Peripheral data transfer size.
+ * @param memory_address Base memory address involved in the transfer.
+ * @param memory_size Memory data transfer size.
+ * @param mode Logical OR of dma_mode_flags
+ *
+ * @see dma_tube_cfg()
+ *
+ * @sideeffect Disables the given DMA channel.
+ * @see dma_xfer_size
+ * @see dma_mode_flags
+ * @see dma_set_num_transfers()
+ * @see dma_set_priority()
+ * @see dma_attach_interrupt()
+ * @see dma_enable()
+ */
+__deprecated
+void dma_setup_transfer(dma_dev       *dev,
+                        dma_channel    channel,
+                        __io void     *peripheral_address,
+                        dma_xfer_size  peripheral_size,
+                        __io void     *memory_address,
+                        dma_xfer_size  memory_size,
+                        uint32         mode) {
+    dma_channel_reg_map *channel_regs = dma_channel_regs(dev, channel);
+
+    dma_disable(dev, channel);  /* can't write to CMAR/CPAR otherwise */
+    channel_regs->CCR = (memory_size << 10) | (peripheral_size << 8) | mode;
+    channel_regs->CMAR = (uint32)memory_address;
+    channel_regs->CPAR = (uint32)peripheral_address;
 }
 
 /*
@@ -351,7 +281,7 @@ void __irq_dma1_channel7(void) {
     dispatch_handler(DMA1, DMA_CH7);
 }
 
-#ifdef STM32_HIGH_DENSITY
+#if defined(STM32_HIGH_DENSITY) || defined(STM32_XL_DENSITY)
 void __irq_dma2_channel1(void) {
     dispatch_handler(DMA2, DMA_CH1);
 }
@@ -365,7 +295,11 @@ void __irq_dma2_channel3(void) {
 }
 
 void __irq_dma2_channel4_5(void) {
-    dispatch_handler(DMA2, DMA_CH4);
-    dispatch_handler(DMA2, DMA_CH5);
+    if ((DMA2_BASE->CCR4 & DMA_CCR_EN) && (DMA2_BASE->ISR & DMA_ISR_GIF4)) {
+        dispatch_handler(DMA2, DMA_CH4);
+    }
+    if ((DMA2_BASE->CCR5 & DMA_CCR_EN) && (DMA2_BASE->ISR & DMA_ISR_GIF5)) {
+        dispatch_handler(DMA2, DMA_CH5);
+    }
 }
 #endif
