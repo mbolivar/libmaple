25863c25761690631bfe30ffc0b6cd65f69378ad
Marti Bolivar <mbolivar@leaflabs.com>
2012-06-11 14:32:11 -0400

    Port examples/test-usart-dma.cpp to use DMA tubes.
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- examples/test-usart-dma.cpp
+++ examples/test-usart-dma.cpp
@@ -22,6 +22,7 @@
 #include <libmaple/dma.h>
 #include <libmaple/usart.h>
 #include <libmaple/gpio.h>
+#include <libmaple/stm32.h>
 
 #include <wirish/wirish.h>
 
@@ -30,10 +31,41 @@
 #define USART USART2
 #define USART_HWSER Serial2
 #define USART_DMA_DEV DMA1
-#define USART_RX_DMA_CHANNEL DMA_CH6
+#define USART_RX_DMA_REQ DMA_REQ_SRC_USART2_RX
+
+#if STM32_MCU_SERIES == STM32_SERIES_F1
+#define USART_RX_DMA_TUBE DMA_CH6
+#elif ((STM32_MCU_SERIES == STM32_SERIES_F2) ||   \
+       (STM32_MCU_SERIES == STM32_SERIES_F4))
+#define USART_RX_DMA_TUBE DMA_S5
+#else
+#error "Unknown STM32 series; can't pick a tube"
+#endif
 
 #define BUF_SIZE 8
 uint8 rx_buf[BUF_SIZE];
+
+// DMA mode flags:
+//
+// - DMA_MINC_MODE so we fill up rx_buf,
+// - DMA_CIRC_MODE so we go back to the beginning of rx_buf when it's
+//   full, and
+// - DMA_TRNS_CMPLT so we get interrupted when the transfer completes.
+uint32 mode_flags = (DMA_MINC_MODE |
+                     DMA_CIRC_MODE |
+                     DMA_TRNS_CMPLT);
+
+// DMA tube configuration for USART RX.
+dma_tube_config usart_tube_config = {
+    &USART->regs->DR, // .tube_per_addr
+    DMA_SIZE_8BITS,   // .tube_per_size
+    rx_buf,           // .tube_mem_addr
+    DMA_SIZE_8BITS,   // .tube_mem_size
+    BUF_SIZE,         // .tube_nr_xfers
+    mode_flags,       // .tube_mode
+    USART_RX_DMA_REQ, // .tube_req_src
+    NULL,             // .series_data
+};
 
 dma_irq_cause irq_cause;
 
@@ -54,8 +86,6 @@ void loop(void) {
     toggleLED();
     delay(100);
 
-    dma_channel_reg_map *ch_regs = dma_channel_regs(USART_DMA_DEV,
-                                                    USART_RX_DMA_CHANNEL);
     if (irq_fired) {
         USART_HWSER.println("** IRQ **");
         irq_fired = 0;
@@ -63,12 +93,8 @@ void loop(void) {
     USART_HWSER.print("[");
     USART_HWSER.print(millis());
     USART_HWSER.print("]\tISR bits: 0x");
-    uint8 isr_bits = dma_get_isr_bits(USART_DMA_DEV, USART_RX_DMA_CHANNEL);
+    uint8 isr_bits = dma_get_isr_bits(USART_DMA_DEV, USART_RX_DMA_TUBE);
     USART_HWSER.print(isr_bits, HEX);
-    USART_HWSER.print("\tCCR: 0x");
-    USART_HWSER.print(ch_regs->CCR, HEX);
-    USART_HWSER.print("\tCNDTR: 0x");
-    USART_HWSER.print(ch_regs->CNDTR, HEX);
     USART_HWSER.print("\tBuffer contents: ");
     for (int i = 0; i < BUF_SIZE; i++) {
         USART_HWSER.print('\'');
@@ -79,7 +105,7 @@ void loop(void) {
     USART_HWSER.println();
     if (isr_bits == 0x7) {
         USART_HWSER.println("** Clearing ISR bits.");
-        dma_clear_isr_bits(USART_DMA_DEV, USART_RX_DMA_CHANNEL);
+        dma_clear_isr_bits(USART_DMA_DEV, USART_RX_DMA_TUBE);
     }
 }
 
@@ -92,13 +118,9 @@ void init_usart(void) {
 /* Configure DMA transmission */
 void init_dma_xfer(void) {
     dma_init(USART_DMA_DEV);
-    dma_setup_transfer(USART_DMA_DEV, USART_RX_DMA_CHANNEL,
-                       &USART->regs->DR, DMA_SIZE_8BITS,
-                       rx_buf,           DMA_SIZE_8BITS,
-                       (DMA_MINC_MODE | DMA_CIRC_MODE | DMA_TRNS_CMPLT));
-    dma_set_num_transfers(USART_DMA_DEV, USART_RX_DMA_CHANNEL, BUF_SIZE);
-    dma_attach_interrupt(USART_DMA_DEV, USART_RX_DMA_CHANNEL, rx_dma_irq);
-    dma_enable(USART_DMA_DEV, USART_RX_DMA_CHANNEL);
+    dma_tube_cfg(USART_DMA_DEV, USART_RX_DMA_TUBE, &usart_tube_config);
+    dma_attach_interrupt(USART_DMA_DEV, USART_RX_DMA_TUBE, rx_dma_irq);
+    dma_enable(USART_DMA_DEV, USART_RX_DMA_TUBE);
 }
 
 void rx_dma_irq(void) {
