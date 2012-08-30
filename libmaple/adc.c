8a4484e820cd7bde5f7f0bb01e789240524589de
Marti Bolivar <mbolivar@leaflabs.com>
2012-08-30 17:15:26 -0400

    Redo adc_read() in terms of new APIs.
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- libmaple/adc.c
+++ libmaple/adc.c
@@ -186,16 +186,12 @@ void adc_set_conv_seq(const adc_dev *dev, const uint8 *channels, uint8 len) {
  * @return conversion result
  */
 uint16 adc_read(const adc_dev *dev, uint8 channel) {
-    adc_reg_map *regs = dev->regs;
-
-    adc_set_reg_seqlen(dev, 1);
-
-    regs->SQR3 = channel;
-    regs->CR2 |= ADC_CR2_SWSTART;
-    while (!(regs->SR & ADC_SR_EOC))
+    adc_set_conv_seq(dev, &channel, 1);
+    adc_start_conv(dev);
+    while (!adc_is_conv_complete(dev))
         ;
 
-    return (uint16)(regs->DR & ADC_DR_DATA);
+    return (uint16)adc_get_data(dev);
 }
 
 /**
